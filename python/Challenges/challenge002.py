2649c502f7ec533207200c214b2a33014a7d10f3
André Euflauzino <andre.euflauzino@gmail.com>
2026-01-07 03:15:23 +0000

    add challenges 2 and 3 on Python 3: Mundo 1

---
+++ python/Challenges/challenge002.py
@@ -0,0 +1,4 @@
+birthday = input("Birthday: ")
+birthday_month = input("Birthday Month: ")
+birthday_year = input("Birthday Year: ")
+print(f"Are you born in {birthday} of {birthday_month}, {birthday_year}.")
---
+++ python/Challenges/challenge003.py
@@ -0,0 +1,3 @@
+first_number = int(input("Fist number: "))
+second_number = int(input("Second number: "))
+print(f"Sum is {first_number + second_number}.")
